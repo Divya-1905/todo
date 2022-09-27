c96c2e144cb74e5b45d10e01c8e892802c20e21e
Divya-1905 <divyasris@venzotechnologies.com>
2022-09-27 11:57:21 +0530

    update

--- todoapp/url.py
+++ todoapp/url.py
@@ -2,7 +2,7 @@ from ast import Delete
 from django.urls import path
 from .views import *
 urlpatterns = [
-      path('index1',index),
+      path('index',index),
     #   path('index2',index2),
     path('update/<str:pk>/', update, name= "update"),
     path('delete_task/<str:pk>/', delete_task, name = "delete_task"),
